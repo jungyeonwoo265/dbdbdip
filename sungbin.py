29a623c587ac1dd2add1a70147c3812a99f0cc5a
tjdqls4965 <tjdqls4965@naver.com>
2023-01-04 11:46:58 +0900

    오전 11시

--- sungbin.py
+++ sungbin.py
@@ -12,7 +12,7 @@ class check(QMainWindow,form_class):
         self.setupUi(self)
         self.setWindowTitle('초등학교 인원')
         self.research_btn.clicked.connect(self.research)
-        self.table.doubleclicked.connect(self.update)
+        self.table.doubleClicked.connect(self.update)
 
         conn = p.connect(host='localhost', port=3306, user='root', password='1234',
                          db='pro', charset='utf8')
@@ -43,12 +43,47 @@ class check(QMainWindow,form_class):
     #     for i in range(len(b)):
     #         for j in range(6):
     #             self.table.setItem(i,j,QTableWidgetItem(str(b[i][j])))
-    def update(self):
-        self.cursor.execute('SELECT * FROM elementary')
+    # def update(self):
+        # year=['2016년','2017년','2018년','2019년','2020년']
+        # nation=[]
+        # self.Header = ['행정구역별', year]
+        #
+        # print("11")
+        # b = self.cursor.fetchall()
+        # print("22")
+        # self.ro = self.table.currentIndex().row()
+        # self.col = self.table.currentIndex().column()
+        # c = b[self.ro][self.col]
+        # print(c)
+
+        # a=self.table.currentRow()
+        # d=self.table.currentColumn()
+        # print(b[0])
+        # print(b[a][d])
+        # print(c)
+        # print(a,d)
+        # f=self.selecteditems()
+
+        # e=self.sele
+        # self.horizontalHeaderItem(d)
+        # self.table.setHorizontalHeaderLabels(self.Header)
+
+
+
+        # a=self.table.currentitem()
+        # print(a.text())
+
+        # self.cursor.execute('SELECT * FROM elementary')
+        # self.cursor.execute(f'SELECT 행정구역별 FROM elementary WHERE 행정구역별 ="{nation}"')
+        # self.cursor.execute(f'update elementary set 행정구역별='{c}'  WHERE 행정구역별 ="{text}"')
+
+
 
     def research(self):
         nation = self.combo_nation.currentText()
         year = self.combo_year.currentText()
+        self.Header = ['행정구역별', year]
+        # self.Header = list(map(str, self.c.fetchone()))
         print(year)
         if nation=="선택안함" and year=="선택안함":
             self.cursor.execute('SELECT * FROM elementary')
@@ -57,16 +92,12 @@ class check(QMainWindow,form_class):
             self.cursor.execute(f'SELECT * FROM elementary where 행정구역별= "{nation}"')
         # 연도만 선택
         elif nation=="선택안함":
-            print('a')
             self.cursor.execute(f'SELECT 행정구역별,{year}년 FROM elementary')
-            print('a')
 
         else:
-            print('b')
             self.cursor.execute(f'SELECT 행정구역별,{year}년 FROM elementary WHERE 행정구역별 ="{nation}"')
 
         b = self.cursor.fetchall()
-        print(b)
         # self.cursor.close()
         self.table.setRowCount(len(b))
         self.table.setColumnCount(len(b[0]))
@@ -74,6 +105,7 @@ class check(QMainWindow,form_class):
         for i in range(len(b)):
             for j in range(len(b[0])):
                 self.table.setItem(i,j,QTableWidgetItem(str(b[i][j])))
+        self.table.setHorizontalHeaderLabels(self.Header)
 
 
 
@@ -83,53 +115,6 @@ class check(QMainWindow,form_class):
 
 
 
-        # b=[]
-        # c = self.liner.text()
-        # if self.combo_nation.currentText() == '선택안함':
-
-        #
-        #
-        # for i in range(0,len(self.a)):
-        #     b.append(self.a[i][idx])
-        # self.d=[]
-        #
-        # count=0
-        # self.index=[]
-        # for i in range(0,len(self.a)):
-        #     count+=1
-            # if c in b[i]:
-            #     self.d.append(self.a[i])
-            #     self.index.append(count)
-            # else:
-            #     pass
-            # count+=1
-        #
-        # self.book_btn.clicked.connect(self.book)
-        # self.table.setRowCount(len(self.d))
-        # self.table.setColumnCount(9)
-        # self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        # for i in range(len(self.d)):
-        #     for j in range(9):
-        #         self.table.setItem(i, j, QTableWidgetItem(self.d[i][j]))
-        # row = self.table.currentIndex().row()
-        # self.t = self.d[row]
-        # self.v = self.a.index(self.t)
-        # self.a[row]=self.a[v]
-
-
-
-    #
-    # #
-    # def exception(self):
-    #     row = self.table.currentIndex().row()
-    #     for i in range(len(self.a)):
-    #         if self.a[i] == self.d[row]:
-    #             print("111111111")
-    #             self.a[i] = self.d[row]
-    #         else:
-    #             print("^1234^^^^^^^^^^^^^^^^^^^^^^")
-    #     self.connect(self.book)
-    # 예매하기 클릭 시
 
 
 
