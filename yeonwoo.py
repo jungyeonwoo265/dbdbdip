21ffa659bc1f4e85dea0934a0d33d34a427d7568
jungyeonwoo265 <83541644+jungyeonwoo265@users.noreply.github.com>
2023-01-04 18:20:27 +0900

    Add files via upload

--- yeonwoo.py
+++ yeonwoo.py
@@ -18,10 +18,10 @@ class WindowClass(QMainWindow, form_class):
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.csv = list()
         self.db = 'elementary'
-        self.initialization()
         self.header = '*'
         self.where = ''
         self.join = ''
+        self.table_header1()
 
         self.research_btn.clicked.connect(self.search)
         self.checkBox.stateChanged.connect(self.choice_db)
@@ -30,48 +30,37 @@ class WindowClass(QMainWindow, form_class):
 
     def search(self):
         if type(self.db) == str:
-            self.requirement()
+            self.condition1()
             self.single_search()
         else:
-            city = self.combo_nation.currentText()
-            year = self.combo_year.currentText()
-            if len(self.db) == 2:
-                print('아직')
-            else:
-                 print('아직')
-            # self.c.execute(f'select {self.header} from {self.db[0]} {self.join} {self.where};')
-            # city = self.combo_nation.currentText()
-            # csv_list = self.c.fetchall()
+            self.condition2()
+            self.multi_search()
 
-            # 2개
-            # select a.행정구역별, a.2016년, b.2016년 from elementary as a
-            # inner join new_marry as b on a.행정구역별 = b.행정구역별 where b.행정구역별 ='경기도';
-
-            # 3개
-            # select a.행정구역별, a.2016년, b.2016년, c.2016년 from elementary as a
-            # inner join new_marry as b on a.행정구역별 = b.행정구역별
-            # inner join solo as c on b.행정구역별 = c.행정구역별 where c.행정구역별 ='경기도';
-
-    def requirement(self):
+    def condition1(self):
         city = self.combo_nation.currentText()
         year = self.combo_year.currentText()
         if city == '선택안함' and year == '선택안함':
             self.header = '*'
+            self.join = ''
             self.where = ''
         elif year == '선택안함':
             self.header = '*'
+            self.join = ''
             self.where = f'where 행정구역별 = "{city}"'
         elif city == '선택안함':
             self.header = f'행정구역별 , {year}년'
+            self.join = ''
             self.where = ''
         else:
             self.header = f'행정구역별 , {year}년'
+            self.join = ''
             self.where = f'where 행정구역별 = "{city}"'
+        self.table_header1()
 
     def single_search(self):
         self.c.execute(f'select {self.header} from {self.db} {self.where};')
-        city = self.combo_nation.currentText()
         csv_list = self.c.fetchall()
+        city = self.combo_nation.currentText()
         if city == '선택안함':
             self.table.setRowCount(len(csv_list) - 1)
             self.table.setColumnCount(len(csv_list[0]))
@@ -95,10 +84,78 @@ class WindowClass(QMainWindow, form_class):
                     else:
                         self.table.setItem(i, j, QTableWidgetItem(csv_list[i][j]))
 
+    def condition2(self):
+        city = self.combo_nation.currentText()
+        year = self.combo_year.currentText()
+        # 2개
+        # select / a.행정구역별, a.2016년, b.2016년 / from elementary as a
+        # / inner join new_marry as b on a.행정구역별 = b.행정구역별 / where a.행정구역별 ='경기도';
+
+        if len(self.db) == 2:
+            if city == '선택안함':
+                self.header = f'{self.db[0]}.행정구역별, {self.db[0]}.{year}년, {self.db[1]}.{year}년'
+                self.join = f'inner join {self.db[1]} on {self.db[0]}.행정구역별 = {self.db[1]}.행정구역별'
+                self.where = f''
+            else:
+                self.header = f'{self.db[0]}.행정구역별, {self.db[0]}.{year}년, {self.db[1]}.{year}년'
+                self.join = f'inner join {self.db[1]} on {self.db[0]}.행정구역별 = {self.db[1]}.행정구역별'
+                self.where = f'where {self.db[1]}.행정구역별 = "{city}"'
+
+        # 3개
+        # select / a.행정구역별, a.2016년, b.2016년, c.2016년 / from elementary as a
+        # / inner join new_marry as b on a.행정구역별 = b.행정구역별
+        # inner join solo as c on b.행정구역별 = c.행정구역별 / where a.행정구역별 ='경기도';
+
+        else:
+            if city == '선택안함':
+                self.header = f'{self.db[0]}.행정구역별, {self.db[0]}.{year}년, {self.db[1]}.{year}년, {self.db[2]}.{year}년'
+                self.join = f'inner join {self.db[1]} on {self.db[0]}.행정구역별 = {self.db[1]}.행정구역별 ' \
+                            f'inner join {self.db[2]} on {self.db[1]}.행정구역별 = {self.db[2]}.행정구역별'
+                self.where = f''
+            else:
+                self.header = f'{self.db[0]}.행정구역별, {self.db[0]}.{year}년, {self.db[1]}.{year}년, {self.db[2]}.{year}년'
+                self.join = f'inner join {self.db[1]} on {self.db[0]}.행정구역별 = {self.db[1]}.행정구역별 ' \
+                            f'inner join {self.db[2]} on {self.db[1]}.행정구역별 = {self.db[2]}.행정구역별'
+                self.where = f'where {self.db[1]}.행정구역별 = "{city}"'
+        self.table_header2()
+
     def multi_search(self):
-        print('멀티')
-    def initialization(self):
-        self.c.execute(f'select * from {self.db};')
+        self.c.execute(f'select {self.header} from {self.db[0]} {self.join} {self.where};')
+        csv_list = self.c.fetchall()
+        city = self.combo_nation.currentText()
+        if city == '선택안함':
+            self.table.setRowCount(len(csv_list) - 1)
+            self.table.setColumnCount(len(csv_list[0]))
+            for i in range(len(csv_list[0])):
+                self.table.setHorizontalHeaderItem(i, QTableWidgetItem(csv_list[0][i]))
+            for i in range(len(csv_list) - 1):
+                for j in range(len(csv_list[0])):
+                    if j == 0:
+                        self.table.setItem(i, j, QTableWidgetItem(csv_list[i + 1][j]))
+                    elif self.db[j-1] != 'solo':
+                        self.table.setItem(i, j, QTableWidgetItem(f'{int(csv_list[i + 1][j]): ,}'))
+                    else:
+                        self.table.setItem(i, j, QTableWidgetItem(csv_list[i + 1][j]))
+        else:
+            self.table.setRowCount(len(csv_list))
+            self.table.setColumnCount(len(csv_list[0]))
+            for i in range(len(csv_list[0])):
+                self.table.setHorizontalHeaderItem(i, QTableWidgetItem(self.csv[0][i]))
+            for i in range(len(csv_list)):
+                for j in range(len(csv_list[0])):
+                    if j == 0:
+                        self.table.setItem(i, j, QTableWidgetItem(csv_list[i][j]))
+                    elif self.db[j-1] != 'solo':
+                        self.table.setItem(i, j, QTableWidgetItem(f'{int(csv_list[i][j]): ,}'))
+                    else:
+                        self.table.setItem(i, j, QTableWidgetItem(csv_list[i][j]))
+
+    def table_header1(self):
+        self.c.execute(f'select {self.header} from {self.db};')
+        self.csv = self.c.fetchall()
+
+    def table_header2(self):
+        self.c.execute(f'select {self.header} from {self.db[0]} {self.join};')
         self.csv = self.c.fetchall()
 
     def choice_db(self):
