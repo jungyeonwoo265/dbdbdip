dc1aa4571158c2635f2a6bb65db94c6399a8474f
tjdqls4965 <121270380+tjdqls4965@users.noreply.github.com>
2023-01-05 15:26:10 +0900

    Add files via upload

--- sungbin.py
+++ sungbin.py
@@ -11,6 +11,9 @@ class check(QMainWindow,form_class):
         self.setWindowTitle('초등학교 인원')
         self.research_btn.clicked.connect(self.research)
         self.table.doubleClicked.connect(self.update)
+        self.add_btn.clicked.connect(self.add_col)
+        self.back_btn.clicked.connect(self.back)
+        self.delete_btn.clicked.connect(self.delete_sel)
 
         conn = p.connect(host='localhost', port=3306, user='root', password='1234',
                          db='pro', charset='utf8')
@@ -27,11 +30,78 @@ class check(QMainWindow,form_class):
         for i in range(len(self.b)):
             for j in range(len(self.b[0])):
                 self.table.setItem(i, j, QTableWidgetItem(str(self.b[i][j])))
-    # def delete(self):
-    #     print("1")
-    def add_file(self):
-        print("2")
-        # ALTER TABLE `pro`.`elementary`
+    def delete_sel(self):
+        conn = p.connect(host='localhost', port=3306, user='root', password='1234',
+                         db='pro', charset='utf8')
+
+        self.cursor = conn.cursor()
+        # self.cursor.execute('SELECT * FROM elementary')
+        # self.b = self.cursor.fetchall()
+        self.ro = self.table.currentIndex().row()
+        self.col = self.table.currentIndex().column()
+        money = self.b[self.ro][self.col]
+        print(self.ro)
+        print(self.b[self.ro][0])
+        print("1")
+        self.cursor.execute("set SQL_SAFE_UPDATES = 0")
+        year = self.combo_year.currentText()
+        if year == "선택안함":
+            self.cursor.execute(f'update elementary set {self.b[0][self.col]}년 = NULL WHERE 행정구역별 ="{self.b[self.ro][0]}"')
+        else:
+            self.cursor.execute(f'update elementary set {year}년 = NULL WHERE 행정구역별 ="{self.b[self.ro][0]}"')
+
+        print("sunssssss")
+        self.cursor.execute("set SQL_SAFE_UPDATES = 1")
+        conn.commit()
+        conn.close()
+        # self.change_btn.connect(self.)
+
+        # print(self.ro,self.col,"!!!!!!")
+
+        self.table.setRowCount(len(self.b))
+        self.table.setColumnCount(len(self.b[0]))
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        for i in range(len(self.b)):
+            for j in range(len(self.b[0])):
+                self.table.setItem(i, j, QTableWidgetItem(str(self.b[i][j])))
+
+    def back(self):
+        conn = p.connect(host='localhost', port=3306, user='root', password='1234',
+                         db='pro', charset='utf8')
+        self.cursor = conn.cursor()
+        self.cursor.execute('ALTER TABLE `pro`.`elementary`DROP COLUMN `2021년`')
+        conn.commit()
+        conn.close()
+
+        self.table.setRowCount(len(self.b))
+        self.table.setColumnCount(len(self.b[0]))
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        for i in range(len(self.b)):
+            for j in range(len(self.b[0])):
+                self.table.setItem(i, j, QTableWidgetItem(str(self.b[i][j])))
+
+    def add_col(self):
+        print("11111111111")
+
+        conn = p.connect(host='localhost', port=3306, user='root', password='1234',
+                         db='pro', charset='utf8')
+        print("2222222222")
+        self.cursor = conn.cursor()
+        self.cursor.execute('ALTER TABLE `pro`.`elementary` ADD COLUMN `2021년` INT NULL DEFAULT NULL AFTER `2020년`')
+        self.cursor.execute('update elementary set 2021년=case WHEN 행정구역별="행정구역별" THEN 2021 WHEN 행정구역별="전국" THEN 2672340 WHEN 행정구역별="서울특별시" THEN 399435 WHEN 행정구역별="부산광역시" THEN 153921 WHEN 행정구역별="대구광역시" THEN 121308 WHEN 행정구역별="인천광역시" THEN 155271 WHEN 행정구역별="광주광역시" THEN 84998 WHEN 행정구역별="대전광역시" THEN 77884 WHEN 행정구역별="울산광역시" THEN 66919 WHEN 행정구역별="세종특별자치시" THEN 30726 WHEN 행정구역별="경기도" THEN 763912  WHEN 행정구역별="강원도" THEN 72373 WHEN 행정구역별="충청북도" THEN 84263 WHEN 행정구역별="충청남도" THEN 118771 WHEN 행정구역별="전라북도" THEN 92914 WHEN 행정구역별="전라남도" THEN 91229 WHEN 행정구역별="경상북도" THEN 127912 WHEN 행정구역별="경상남도" THEN 189176  WHEN 행정구역별="제주특별자치도" THEN 41328  END')
+
+        print("333333")
+
+        conn.commit()
+        conn.close()
+
+        self.table.setRowCount(len(self.b))
+        self.table.setColumnCount(len(self.b[0]))
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        for i in range(len(self.b)):
+            for j in range(len(self.b[0])):
+                self.table.setItem(i, j, QTableWidgetItem(str(self.b[i][j])))
+
 
 
 
@@ -53,6 +123,8 @@ class check(QMainWindow,form_class):
         for i in f:
             print(i.text())
         print(f[0].text(), "!!!!!!!!!!")
+        print(self.b[0][self.col],"^^^^^")
+        print(self.col)
         print(self.b[self.ro][0], "@@@@")
         sung=int(self.liner.text())
         # year=['2016년','2017년','2018년','2019년','2020년']
